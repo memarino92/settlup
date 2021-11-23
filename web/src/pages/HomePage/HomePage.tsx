abaf46afd77662c710f6d56d29ff9bf54b1d0e73
memarino92 <memarino92@gmail.com>
2021-11-22 21:20:56 -0500

    removed test flag

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,6 @@
 import ExpenseItemsCell from 'src/components/ExpenseItemsCell'
-import { useFlag } from '@unleash/proxy-client-react'
 
 const HomePage = () => {
-  if (useFlag('UnderConstruction')) {
-    return <h1>Under Construction</h1>
-  }
   return (
     <>
       <ExpenseItemsCell />
